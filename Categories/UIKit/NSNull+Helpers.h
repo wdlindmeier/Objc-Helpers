15f2d156a08a6e4af8e10a9e465ccb2642bbf408
wdlindmeier <wdlindmeier@gmail.com>
2009-12-22 00:53:18 -0800

    Removed private API call and other minor updates

---
+++ Categories/UIKit/NSNull+Helpers.h
@@ -0,0 +1,16 @@
+//
+//  NSNull+Helpers.h
+//  tweak
+//
+//  Created by William Lindmeier on 12/1/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSNull(Helpers)
+
+- (BOOL)isEmpty;
+
+@end
