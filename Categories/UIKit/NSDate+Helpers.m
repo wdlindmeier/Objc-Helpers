906642d68ec27216f99a79e13f705f4ea58f0549
wdlindmeier <wdlindmeier@gmail.com>
2010-11-03 23:34:55 -0700

    triple pass on JSON strings of various formats

--- Categories/UIKit/NSDate+Helpers.m
+++ Categories/UIKit/NSDate+Helpers.m
@@ -73,26 +73,33 @@ static NSDateFormatter *sharedFormatter;
 
 + (NSDate *)dateFromJSONFormattedString:(NSString *)JSONstring
 {
-	// Strip any +dd:dd or -dd:dd or Z from the end of the string
+	NSDate *parsed = nil;
 	if([JSONstring isNotBlank]){
-		int stringLength = [JSONstring length];
-		NSString *stringSansTZ = [JSONstring stringByReplacingOccurrencesOfString:@"(Z|(\\+|\\-)\\d\\d:\\d\\d)$" 
-																	   withString:@"" 
-																		  options:NSRegularExpressionSearch 
-																			range:NSMakeRange(0, stringLength)];
-		NSDate *parsed = nil;
 		NSDateFormatter *dateFormatter = [NSDate sharedFormatter];
-		//2011-09-17T03:36:27
-		[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss"];
-		@try {
-			parsed = [dateFormatter dateFromString:stringSansTZ];
+		[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+		
+		@try { parsed = [dateFormatter dateFromString:JSONstring];}
+		@catch (NSException * e) {}
+		
+		if(!parsed){
+			[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+			@try { parsed = [dateFormatter dateFromString:JSONstring]; }
+			@catch (NSException * e) {}
+
+			if(!parsed){
+				int stringLength = [JSONstring length];
+				NSString *stringSansTZ = [JSONstring stringByReplacingOccurrencesOfString:@"(Z|(\\+|\\-)\\d\\d:\\d\\d)$" 
+																			   withString:@"" 
+																				  options:NSRegularExpressionSearch 
+																					range:NSMakeRange(0, stringLength)];
+				
+				[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss"];
+				@try { parsed = [dateFormatter dateFromString:stringSansTZ];}
+				@catch (NSException * e) {}
+			}
 		}
-		@catch (NSException * e) {
-			NSLog(@"NSDate Error: Could not parse date from %@", stringSansTZ);
-		}	
-		return parsed;
 	}
-	return nil;
+	return parsed;
 }
 	
 @end
